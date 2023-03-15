97afa7e12cc45fcd0ccf8cf64eadc36933532cb7
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-15 12:27:52 +0100

    feat(@simplyui/react): update Color global type

--- packages/simplyui-react/src/types/index.ts
+++ packages/simplyui-react/src/types/index.ts
@@ -1,24 +1,3 @@
 export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-export type Color =
-  | 'blue'
-  | 'brown'
-  | 'crimson'
-  | 'cyan'
-  | 'grass'
-  | 'green'
-  | 'indigo'
-  | 'orange'
-  | 'pink'
-  | 'plum'
-  | 'purple'
-  | 'red'
-  | 'teal'
-  | 'tomato'
-  | 'violet';
-
-export type BrightColor = 'sky' | 'mint' | 'lime' | 'yellow' | 'amber';
-
-export type GrayColor = 'gray' | 'mauve' | 'slate' | 'sage' | 'olive' | 'sand';
-
-export type MetalColor = 'gold' | 'bronze';
+export type Color = 'neutral' | 'primary' | 'secondary' | 'success' | 'warning' | 'error';
