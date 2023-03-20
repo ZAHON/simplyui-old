29b3c7a04d29e0dec44d28f51d92c9fd16a03f09
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-20 14:51:07 +0100

    feat(@simplyui/react): update Rounded global type

--- packages/simplyui-react/src/types/index.ts
+++ packages/simplyui-react/src/types/index.ts
@@ -1,5 +1,5 @@
 export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-export type Rounded = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full';
+export type Rounded = 'none' | 'full';
 
 export type Color = 'neutral' | 'primary' | 'secondary' | 'success' | 'warning' | 'error';
