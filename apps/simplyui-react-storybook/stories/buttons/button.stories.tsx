fc8bfe849d3044d4b0d85db8535e9f0f5870b17a
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-20 15:03:15 +0100

    feat(simplyui-react-storybook): update stories for Button component

--- apps/simplyui-react-storybook/stories/buttons/button.stories.tsx
+++ apps/simplyui-react-storybook/stories/buttons/button.stories.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { BellIcon } from '../../icons';
 
 import { Button } from '@simplyui/react/src';
-
-function DocumentPlusIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-      className="w-4 h-4"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-      />
-    </svg>
-  );
-}
 
 export default {
   title: 'Buttons/Button',
@@ -45,20 +27,7 @@ export default {
     color: { control: 'select' },
     rounded: {
       control: 'select',
-      options: [
-        undefined,
-        'none',
-        'xs',
-        'sm',
-        'md',
-        'lg',
-        'xl',
-        '2xl',
-        '3xl',
-        'squared',
-        'half',
-        'full',
-      ],
+      options: [undefined, 'none', 'full'],
     },
     type: { control: 'select' },
     loaderPosition: { control: 'select' },
@@ -121,16 +90,16 @@ LoadingWithLoadingText.args = {
 
 export const WithLeftIcon = Template.bind({});
 WithLeftIcon.args = {
-  leftIcon: <DocumentPlusIcon />,
+  leftIcon: <BellIcon />,
 };
 
 export const WithRightIcon = Template.bind({});
 WithRightIcon.args = {
-  rightIcon: <DocumentPlusIcon />,
+  rightIcon: <BellIcon />,
 };
 
 export const WithLeftAndRightIcon = Template.bind({});
 WithLeftAndRightIcon.args = {
-  leftIcon: <DocumentPlusIcon />,
-  rightIcon: <DocumentPlusIcon />,
+  leftIcon: <BellIcon />,
+  rightIcon: <BellIcon />,
 };
