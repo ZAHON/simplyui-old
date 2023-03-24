2128f98dd29614571fce0a971c5bd014ddaebc62
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-24 18:42:13 +0100

    feat(simplyui-react-storybook): change tailwindcss config

--- apps/simplyui-react-storybook/tailwind.config.cjs
+++ apps/simplyui-react-storybook/tailwind.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
   content: [
     '../../packages/simplyui-react/src/**/*.{js,ts,jsx,tsx}',
     './stories/**/*.{js,ts,jsx,tsx}',
