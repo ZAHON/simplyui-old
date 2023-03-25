6f3ba52b5f68deb9f010c26b9f102496b14cb5fc
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-25 23:39:12 +0100

    feat(simplyui-react-storybook): update stories for Label component

--- apps/simplyui-react-storybook/stories/inputs/label.stories.tsx
+++ apps/simplyui-react-storybook/stories/inputs/label.stories.tsx
@@ -9,16 +9,37 @@ export default {
   args: {
     htmlFor: 'input-id',
     children: 'Label',
+    description: '',
+    disabled: false,
     withAsterisk: false,
     size: 'md',
+    color: 'default',
   },
   argTypes: {
     size: { control: 'select' },
-    className: { control: false },
+    color: { control: 'select' },
+    rootClassName: { control: false },
+    labelClassName: { control: false },
     asteriskClassName: { control: false },
+    descriptionClassName: { control: false },
   },
 } as ComponentMeta<typeof Label>;
 
 const Template: ComponentStory<typeof Label> = (args) => <Label {...args} />;
 
 export const Default = Template.bind({});
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+export const WithAsterisk = Template.bind({});
+WithAsterisk.args = {
+  withAsterisk: true,
+};
+
+export const WithDescription = Template.bind({});
+WithDescription.args = {
+  description: 'Label description',
+};
