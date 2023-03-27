ad7838235ddf556b156d28afd351a542b4b2e423
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-27 21:34:14 +0200

    feat(@simplyui/react): remove Label component

--- packages/simplyui-react/src/components/label/label-description/label-description.types.ts
+++
@@ -1,7 +0,0 @@
-import type { HTMLAttributes, ReactNode } from 'react';
-import type { Color } from '../../../types';
-
-export interface LabelDescriptionProps extends HTMLAttributes<HTMLSpanElement> {
-  color?: Exclude<Color, 'neutral'> | 'default';
-  children: ReactNode;
-}
--- packages/simplyui-react/src/index.ts
+++ packages/simplyui-react/src/index.ts
@@ -2,6 +2,5 @@ export * from './components/action-icon';
 export * from './components/button';
 export * from './components/close-button';
 export * from './components/field-label';
-export * from './components/label';
 export * from './components/loader';
 export * from './components/switch';
