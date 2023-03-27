caad4c2005aaddaa8aa686dacd6721017b28b658
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-03-27 20:02:46 +0200

    chore(eslint-config-custom): add eslint-plugin-jest-dom and eslint-plugin-testing-library

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,13 @@
 module.exports = {
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'testing-library', 'jest-dom'],
   extends: [
     'next',
     'turbo',
     'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:storybook/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
   ],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
