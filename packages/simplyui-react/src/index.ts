5adbd6f4df577542d67e090874e39354ca78a80b
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-01 01:28:54 +0200

    feat(@simplyui/react): remove exports for components that require refactoring from the main file

--- packages/simplyui-react/src/index.ts
+++ packages/simplyui-react/src/index.ts
@@ -1,6 +1,3 @@
-export * from './components/action-icon';
-export * from './components/button';
-export * from './components/close-button';
 export * from './components/label';
 export * from './components/loader';
 export * from './components/switch';
