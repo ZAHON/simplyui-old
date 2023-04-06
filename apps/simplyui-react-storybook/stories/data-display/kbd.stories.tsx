cb81a455c72ec8dc756cd32f8dd657a4c6f9b972
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-06 11:00:34 +0200

    feat(simplyui-react-storybook): add stories for Kbd component

---
+++ apps/simplyui-react-storybook/stories/data-display/kbd.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Kbd } from '@simplyui/react/src';
+
+export default {
+  title: 'Data display/Kbd',
+  component: Kbd,
+  args: {
+    size: 'sm',
+    rounded: 'xs',
+  },
+  argTypes: {
+    size: { control: 'select' },
+    rounded: { control: 'select' },
+    className: { control: false },
+    style: { control: false },
+  },
+} as ComponentMeta<typeof Kbd>;
+
+const Template: ComponentStory<typeof Kbd> = (args) => <Kbd {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: '⌘',
+};
