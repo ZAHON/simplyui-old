16042a813969f10a52c4fbd75c17968c8c398cad
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-08 08:54:24 +0200

    feat(simplyui-react-storybook): add bookmark icon

--- apps/simplyui-react-storybook/icons/index.ts
+++ apps/simplyui-react-storybook/icons/index.ts
@@ -1,3 +1,4 @@
 export { ArrowsRightLeftIcon } from './arrows-right-left-icon';
 export { BellIcon } from './bell-icon';
 export { BellSlashIcon } from './bell-slash-icon';
+export { BookmarkIcon } from './bookmark-icon';
