b555a3217d775f8732ef7e0b134bf9aab20e744f
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-08 08:57:27 +0200

    feat(simplyui-react-storybook): add a11y attributes for icons

--- apps/simplyui-react-storybook/icons/arrows-right-left-icon.tsx
+++ apps/simplyui-react-storybook/icons/arrows-right-left-icon.tsx
@@ -6,6 +6,8 @@ export function ArrowsRightLeftIcon(props: IconProps) {
 
   return (
     <svg
+      focusable="false"
+      aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       fill="currentColor"
