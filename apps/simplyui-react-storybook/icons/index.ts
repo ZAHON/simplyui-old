3e58fb75fd01b637808d83e0ffa7e8147b5ffaaa
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-09 04:05:30 +0200

    feat(simplyui-react-storybook): add wrenchk icon

--- apps/simplyui-react-storybook/icons/index.ts
+++ apps/simplyui-react-storybook/icons/index.ts
@@ -2,3 +2,4 @@ export { ArrowsRightLeftIcon } from './arrows-right-left-icon';
 export { BellIcon } from './bell-icon';
 export { BellSlashIcon } from './bell-slash-icon';
 export { BookmarkIcon } from './bookmark-icon';
+export { WrenchkIcon } from './wrench-icon';
