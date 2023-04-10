b956d41b3ac974f0bec81cfa31750a220dd69ff4
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-10 06:32:45 +0200

    feat(@simplyui/tailwind-plugin): update border radius values

--- packages/simplyui-tailwind-plugin/src/border-radius.js
+++ packages/simplyui-tailwind-plugin/src/border-radius.js
@@ -2,14 +2,11 @@ module.exports = {
   borderRadius: {
     inherit: 'inherit',
     none: '0',
-    xs: '7px',
-    sm: '9px',
-    md: '12px',
-    lg: '14px',
-    xl: '18px',
-    '2xl': '24px',
-    '3xl': '32px',
-    squared: '33%',
+    xs: '0.4375rem', // 7px
+    sm: '0.5625rem', // 9px
+    md: '0.75rem', // 12px
+    lg: '0.875rem', // 14px
+    xl: '1.125rem', // 18px
     full: '9999px',
   },
 };
