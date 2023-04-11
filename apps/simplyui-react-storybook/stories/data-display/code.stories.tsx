6955c070edf172e8edb99e5c72171734c42c497a
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-11 13:00:31 +0200

    feat(simplyui-react-storybook): update stories for Code component

--- apps/simplyui-react-storybook/stories/data-display/code.stories.tsx
+++ apps/simplyui-react-storybook/stories/data-display/code.stories.tsx
@@ -9,14 +9,16 @@ export default {
   args: {
     children: 'npm install @simplyui/react',
     size: 'md',
-    color: 'text',
+    color: 'neutral',
     backgroundColor: 'neutral',
+    accent: undefined,
     rounded: 'xs',
   },
   argTypes: {
     size: { control: 'select' },
     color: { control: 'select' },
     backgroundColor: { control: 'select' },
+    accent: { control: 'select' },
     rounded: { control: 'select' },
     className: { control: false },
     style: { control: false },
@@ -33,3 +35,23 @@ export default {
 const Template: ComponentStory<typeof Code> = (args) => <Code {...args} />;
 
 export const Default = Template.bind({});
+
+export const TopAccent = Template.bind({});
+TopAccent.args = {
+  accent: 'top',
+};
+
+export const RightAccent = Template.bind({});
+RightAccent.args = {
+  accent: 'right',
+};
+
+export const BottomAccent = Template.bind({});
+BottomAccent.args = {
+  accent: 'bottom',
+};
+
+export const LeftAccent = Template.bind({});
+LeftAccent.args = {
+  accent: 'left',
+};
