c1ba9ead08fcd035cee764ede31ab3efdf729c22
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-14 00:18:50 +0200

    feat(simplyui-react-storybook): add exclamation triangle icon

--- apps/simplyui-react-storybook/icons/index.ts
+++ apps/simplyui-react-storybook/icons/index.ts
@@ -2,4 +2,5 @@ export { ArrowsRightLeftIcon } from './arrows-right-left-icon';
 export { BellIcon } from './bell-icon';
 export { BellSlashIcon } from './bell-slash-icon';
 export { BookmarkIcon } from './bookmark-icon';
+export { ExclamationTriangleIcon } from './exclamation-triangle-icon';
 export { WrenchkIcon } from './wrench-icon';
