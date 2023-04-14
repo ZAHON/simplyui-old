7217168cf45f62fe77303d50b1251df9e935900c
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-14 03:14:50 +0200

    feat(@simplyui/react): remove Switch component

--- packages/simplyui-react/src/components/switch/index.ts
+++
@@ -1,3 +0,0 @@
-export type { SwitchProps } from './switch.types';
-
-export { Switch } from './switch';
--- packages/simplyui-react/src/index.ts
+++ packages/simplyui-react/src/index.ts
@@ -9,4 +9,3 @@ export * from './components/label';
 export * from './components/loader';
 export * from './components/progress';
 export * from './components/skeleton';
-export * from './components/switch';
