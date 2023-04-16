8ca64ee74b89c76193f766caed69d40e889b0731
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-16 22:13:13 +0200

    feat(simplyui-react-storybook): add new icons

--- apps/simplyui-react-storybook/icons/index.ts
+++ apps/simplyui-react-storybook/icons/index.ts
@@ -2,5 +2,7 @@ export { ArrowsRightLeftIcon } from './arrows-right-left-icon';
 export { BellIcon } from './bell-icon';
 export { BellSlashIcon } from './bell-slash-icon';
 export { BookmarkIcon } from './bookmark-icon';
+export { ChevronDownIcon } from './chevron-down-icon';
+export { CodeBracketIcon } from './code-bracket-icon';
 export { ExclamationTriangleIcon } from './exclamation-triangle-icon';
 export { WrenchkIcon } from './wrench-icon';
