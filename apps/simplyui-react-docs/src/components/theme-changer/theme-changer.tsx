61154c90837df3268bf05a65f70b1c0ce9a63834
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-20 20:27:07 +0200

    feat(simplyui-react-docs): update ThemeChanger component

--- apps/simplyui-react-docs/src/components/theme-changer/theme-changer.tsx
+++ apps/simplyui-react-docs/src/components/theme-changer/theme-changer.tsx
@@ -1,10 +1,16 @@
 'use client';
+import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { IconButton } from '@simplyui/react';
 import { IconSunHigh } from '@tabler/icons-react';
 
 export function ThemeChanger() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const label = `Switch to ${theme} theme`;
 
@@ -18,6 +24,10 @@ export function ThemeChanger() {
         setTheme('light');
       }
     }
+  }
+
+  if (!mounted) {
+    return null;
   }
 
   return (
