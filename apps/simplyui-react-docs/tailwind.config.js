8ccc841675f72dd7a3337381217ef90f0f1c968b
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-21 00:46:44 +0200

    feat(simplyui-react-docs): update content in tailwind.config.js

--- apps/simplyui-react-docs/tailwind.config.js
+++ apps/simplyui-react-docs/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
-    '../../packages/simplyui-react/dist/**/*.js',
+    '../../packages/simplyui-react/src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
