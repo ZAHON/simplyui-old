720611b46b2f69665cea4fb02c29013510209c8f
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-04-20 20:12:42 +0200

    feat(simplyui-react-docs): add Providers component to HomeLayout

--- apps/simplyui-react-docs/src/app/(home)/layout.tsx
+++ apps/simplyui-react-docs/src/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import '../../styles/globals.css';
 import type { ReactNode } from 'react';
+import { Providers } from '@/components';
 
 export const metadata = {
   title: 'Simplyui React',
@@ -14,8 +15,10 @@ export default function HomeLayout(props: HomeLayoutProps) {
   const { children } = props;
 
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" data-theme="light" suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
